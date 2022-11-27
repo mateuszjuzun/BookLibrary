8b92aeac07ad4b616eb055521a80be3c1313ef92
Mateusz <mateusz.juzun@gmail.com>
2022-11-27 20:50:34 +0100

    endpoint added

--- src/main/java/pl/springapp/booklibrary/BookController.java
+++ src/main/java/pl/springapp/booklibrary/BookController.java
@@ -1,10 +1,16 @@
 package pl.springapp.booklibrary;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 public class BookController {
+
+@Autowired
+BookRepository bookRepository;
 
 
     @GetMapping("/test")
@@ -13,6 +19,9 @@ public int test() {
 }
 
 @GetMapping("/books")
+public List<Book> getAll(){
+   return bookRepository.getAll();
+}
 
 
 }
